8103aa7ea126c6e9e5cc92709f1b8a0b0467d76d
Austin Han Wang <wanghan33@gmail.com>
2021-05-20 21:57:00 +1000

    working version

--- lambda_functions/ingestion-trigger.py
+++ lambda_functions/ingestion-trigger.py
@@ -9,30 +9,19 @@ def lambda_handler(event, context):
     
     def get_name_of_place(lat, long):
         '''queries google place API to match lat long with name of place'''
-
-        URL = ("https://maps.googleapis.com/maps/api/place/nearbysearch/json?radius=25&location=" +
-            str(lat) + "," + str(long) + "&key=" + API_KEY)
-        
-        response = urllib.request.urlopen(URL).read()
-        data = json.loads(response.decode('utf-8'))
-        
-        if len(data['results']) > 0:
-            print('got name string')
-            return data['results'][0]['name']
-        else:
-            print('returned no name string')
-            return ""
+        return lat+long
         
     s3 = boto3.client("s3")
     
     if event:
         file_obj = event["Records"][0]
         filename = str(file_obj['s3']['object']['key'])
-        fileObj = s3.get_object(Bucket='event-pipe-data', Key=filename)
+        fileObj = s3.get_object(Bucket='log-pipe-data', Key=filename)
         bytestream = BytesIO(fileObj['Body'].read())
         file_content = GzipFile(mode='rb', fileobj=bytestream).read().decode('utf-8')
         
         file_content = file_content.replace('][', ',')
+        print(file_content)
         
         data = json.loads(file_content)
         
@@ -41,7 +30,7 @@ def lambda_handler(event, context):
         year = str(now.year)
         month = str(now.month)
         day = str(now.day)
-        bucket_name = 'visitation-labeled'
+        bucket_name = 'visitation-labeled-austin-test'
         file_name = now_string.replace('.','_')
         file_name = file_name.replace(':', '_')
         file_name = file_name.replace('-', '_')
@@ -65,4 +54,3 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
     }
-
